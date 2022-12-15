8db55e0623e19e7052c344bc1a4d784d18dbffd2
ilarioforcherio <ilarioforcherio@gmail.com>
2022-12-15 18:20:35 +0100

    push

--- api/index.php
+++ api/index.php
@@ -3,19 +3,32 @@
 include_once __DIR__ . '/../db/index.php';
 
 
-$arrayDischi = [];
+// $arrayDischi = [];
+
+$arrayGeneri = [];
 
 
-foreach ($database as $object ) {
-    $arrayDischi[] = $object;
-}
+if( !empty($_GET) && !empty($_GET['genre'])){
+   
+    foreach($database as $elem){
+    
+    if($elem['genre'] == $_GET['genre']){
+        $arrayGeneri[] = $elem;
+    }
+   }
+
+}else{
+    $arrayGeneri =  $database;
+};
+
+
 
 
 
 
 header('Content-type: application/json');
 
-echo json_encode( $arrayDischi );
+echo json_encode( $arrayGeneri );
 
 
 ?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,29 @@
+
+
 var app = new Vue({
     el: '#app',
     data: {
+
     arrayDischi : [],
+    arrayGeneri:[],
+    selectGenere:'',
+   
     },
     mounted() {
-      axios.get('./api/index.php')
+     
+        axios.get('./api/index.php')
         .then((res) => {
-         console.log(res.data)
-         this.arrayDischi = res.data
+         //console.log(res.data)
+         this.arrayDischi = res.data;
+
+
+         this.arrayDischi.forEach((element) => {
+
+            console.log(element)
+            if(!this.arrayGeneri.includes(element.genre)){
+                this.arrayGeneri.push(element.genre)
+            }
+          });
   
         })
     },
@@ -15,7 +31,14 @@ var app = new Vue({
     
     },
     methods: {
+     
+         ricercaGeneri(){
+          axios.get(`./api/index.php?genre=${this.selectGenere} `).then((resp)=>{
+            console.log(resp)
+            this.arrayDischi = resp.data
+          })
+         }
 
-    }
-  })
+    },
+  });
   
